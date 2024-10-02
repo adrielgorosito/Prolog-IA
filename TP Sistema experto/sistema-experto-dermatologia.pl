0e4195d9018e349600499cc7e524d148458ee5f5
Adriel Gorosito <adrielgorosito14@gmail.com>
2024-10-01 22:15:38 -0300

    Final enfermedades con misma zona y problema

--- TP Sistema experto/sistema-experto-dermatologia.pl
+++ TP Sistema experto/sistema-experto-dermatologia.pl
@@ -15,28 +15,14 @@ preguntar_filtros(Zona, Problema) :-
 % Consulta de síntomas
 preguntar_sintomas(Zona, Problema, Lista_sintomas_usuario) :-
     findall(X, enfermedad(_, Zona, Problema, X), Lista_sintomas_enfermedades),
-    desglosar_sintomas(Lista_sintomas_enfermedades, Lista_sintomas_usuario, []).
-	% preguntar_lista_sintomas(Sintomas, [], Lista_sintomas_usuario).
-preguntar_sintomas(_, _, _) :-
-    write('No se encontraron coincidencias de enfermedades con la zona y problema indicados.'),
-    nl,
-    write('Consulte con su dermatólogo de confianza.'), nl,
-    fail.
+    desglosar_sintomas(Lista_sintomas_enfermedades, [], Lista_sintomas_usuario).
 
-desglosar_sintomas([], Lista_sintomas_usuario, Lista_acumulada) :-
-    write(Lista_acumulada), nl,
-    Lista_sintomas_usuario = Lista_acumulada.
+desglosar_sintomas([], Lista_sintomas_usuario, Lista_sintomas_usuario).
 
-desglosar_sintomas([H|T], Lista_sintomas_usuario, Lista_acumulada) :-
-    preguntar_lista_sintomas(H, [], Sintomas),
-    agregar_sintomas(Sintomas, Lista_acumulada, Nueva_lista_acumulada),
-    desglosar_sintomas(T, Lista_sintomas_usuario, Nueva_lista_acumulada).
+desglosar_sintomas([H|T], Lista_temp, Lista_sintomas_usuario) :-
+    preguntar_lista_sintomas(H, Lista_temp, Nueva_lista),
+    desglosar_sintomas(T, Nueva_lista, Lista_sintomas_usuario).
 
-agregar_sintomas([], Lista, Lista).
-agregar_sintomas([S|Resto], Lista, [S|Lista_Nueva]) :-
-    agregar_sintomas(Resto, Lista, Lista_Nueva).
-
-% Consulta de síntomas
 preguntar_lista_sintomas([], Lista, Lista).
 preguntar_lista_sintomas([Sintoma|Resto], Lista_temp, Lista_final) :- 
     consultar_sintoma(Sintoma, Lista_temp, Nueva_lista), 
@@ -52,32 +38,56 @@ procesar_respuesta(_, no, Lista_temp, Lista_temp).
 
 % Diagnóstico
 diagnosticar(Zona, Problema, Lista_final) :-
-    findall(X, enfermedad(X, Zona, Problema, _), Enfermedad),
-    buscar_sintomas(Enfermedad, Lista_sintomas),
-   	contar_sintomas(Lista_sintomas, Cant_sintomas_enfermedad),
-    contar(Lista_final, Cant_sintomas_usuario),
-    diagnostico_final(Enfermedad, Cant_sintomas_enfermedad, Cant_sintomas_usuario).
+    findall(X, enfermedad(X, Zona, Problema, _), Enfermedades),
+    Enfermedades \= [],
+    proceso_diagnostico(Enfermedades, Lista_final).
+diagnosticar(Zona, Problema, _) :-
+    findall(X, enfermedad(X, Zona, Problema, _), Enfermedades),
+    Enfermedades = [],
+    write('No se encontró ninguna enfermedad que coincida con los síntomas proporcionados.'), nl.
 
-buscar_sintomas([H|_], Lista_sintomas) :- 
-    findall(X, enfermedad(H, _, _, X), Lista_sintomas).
-% Sólo contemplamos la búsqueda de los sintomas de una sola enfermedad, habría q hacerlo por enfermedad
+proceso_diagnostico([], _) :-
+    write('No se pudo realizar un diagnóstico preciso.'), nl.
 
-contar_sintomas([H|_], Cant) :- contar(H, Cant).
+proceso_diagnostico([H|T], Lista_final) :-
+    buscar_sintomas(H, Lista_sintomas_enfermedad),
+    contar(Lista_sintomas_enfermedad, Cant_sintomas_enfermedad),
+    write("CANT SÍNTOMAS ENF: "), write(Cant_sintomas_enfermedad), nl, nl,
+    filtrar_sintomas(Lista_final, Lista_sintomas_enfermedad, Lista_nueva),
+    contar(Lista_nueva, Cant_sintomas_usuario),
+    write("CANT SÍNTOMAS USU: "), write(Cant_sintomas_usuario), nl, nl,
+    diagnostico_final(H, Cant_sintomas_enfermedad, Cant_sintomas_usuario),
+    proceso_diagnostico(T, Lista_final).
+
+buscar_sintomas(Enfermedad, Lista_sintomas) :- 
+    enfermedad(Enfermedad, _, _, Lista_sintomas).
+
 contar([], 0).
-contar([_|T], Cant):- contar(T, Cant_cola), Cant is Cant_cola + 1.
+contar([_|T], Cant) :- contar(T, Cant_cola), Cant is Cant_cola + 1.
 
+filtrar_sintomas([], _, []).
+filtrar_sintomas([H|T], Lista_sintomas_enfermedad, [H|Lista_nueva]) :-
+    pertenece(H, Lista_sintomas_enfermedad),
+    filtrar_sintomas(T, Lista_sintomas_enfermedad, Lista_nueva).
+filtrar_sintomas([_|T], Lista_sintomas_enfermedad, Lista_nueva) :-
+    filtrar_sintomas(T, Lista_sintomas_enfermedad, Lista_nueva).
+
+pertenece(X, [X|_]). 
+pertenece(X, [_|T]) :- pertenece(X, T).
+
+% Lógica para el diagnóstico final
 diagnostico_final(Enfermedad, Cant_sintomas_enfermedad, Cant_sintomas_usuario) :-
     X is Cant_sintomas_enfermedad / 2,
     Y is Cant_sintomas_usuario - X,
     Y >= 0, 
     write('Usted puede llegar a tener: '), write(Enfermedad), nl,
-    write('Consulte con su dermatólogo de confianza.').
+    write('Consulte con su dermatólogo de confianza.'), nl.
 
 diagnostico_final(_, Cant_sintomas_enfermedad, Cant_sintomas_usuario) :-
     X is Cant_sintomas_enfermedad / 2,
     Y is Cant_sintomas_usuario - X,
     Y < 0, 
-    write('Sus síntomas no coinciden con ninguna enfermedad. De todas formas, consulte con su dermatólogo de confianza.').
+    write('Sus síntomas no coinciden con ninguna enfermedad. De todas formas, consulte con su dermatólogo de confianza.'), nl.
 
 % Interfaz de usuario
 inicio :-
@@ -89,5 +99,5 @@ inicio :-
 
 
 % ToDo:
-% 1. Modificar la lógica de buscar_sintomas y lo que sigue para dos o mas enfermedades.
-% 2. Ver el tema de si se repiten los síntomas para dos enfermedades.
+% 1. Ver impresión de enfermedades
+% 2. Ver si dos o mas enfermedades tienen uno o mas sintomas
